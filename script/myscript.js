980f70b220058c5a427e0bb03966e0ed9cecefeb
milosnovakovic <milosnovakovic91@gmail.com>
2026-02-02 17:36:15 +0100

    fixed scroll to top

--- script/myscript.js
+++ script/myscript.js
@@ -8,7 +8,31 @@ $(document).ready(function () {
     });
 });
 
+(function () {
+    var toTopVisible = false;
+    var ticking = false;
+    var SHOW_AT = 120;
+    var HIDE_AT = 80;
 
+    function updateToTop() {
+        var scroll = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+        var shouldShow = scroll >= (toTopVisible ? HIDE_AT : SHOW_AT);
+        if (shouldShow !== toTopVisible) {
+            toTopVisible = shouldShow;
+            $(".to-top").toggleClass("on", toTopVisible);
+        }
+        ticking = false;
+    }
+
+    function onScroll() {
+        if (!ticking) {
+            ticking = true;
+            requestAnimationFrame(updateToTop);
+        }
+    }
+
+    $(window).on("scroll", onScroll);
+})();
 
 if (typeof $.fn.fancybox !== 'undefined') {
     $('[data-fancybox]').fancybox({
